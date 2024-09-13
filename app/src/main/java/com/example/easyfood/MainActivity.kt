ed336c99a4389d650e06b9b2272fcd8fdec93113
SK Nur Ahmad <sknurahmad2@gmail.com>
2024-09-13 20:58:19 +0530

    fragment set up done sucessfully

---
+++ app/src/main/java/com/example/easyfood/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.easyfood
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.easyfood.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigation = binding.bottomNavBar
+        val navController = Navigation.findNavController(this,R.id.host_fragment)
+
+        NavigationUI.setupWithNavController(bottomNavigation,navController)
+    }
+}
