13e21951021fc30a70445074f3cd6d516ea8f39f
SK Nur Ahmad <sknurahmad2@gmail.com>
2024-10-27 12:06:35 +0530

    home fragment random meal and meal activity page all done

--- app/src/main/java/com/example/easyfood/activities/MealActivity.kt
+++ app/src/main/java/com/example/easyfood/activities/MealActivity.kt
@@ -1,31 +1,76 @@
 package com.example.easyfood.activities
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.easyfood.R
 import com.example.easyfood.databinding.ActivityMealBinding
+import com.example.easyfood.fragment.HomeFragment.Companion.CATEGORY_NAME
+import com.example.easyfood.fragment.HomeFragment.Companion.MEAL_AREA
 import com.example.easyfood.fragment.HomeFragment.Companion.MEAL_ID
+import com.example.easyfood.fragment.HomeFragment.Companion.MEAL_INS
+import com.example.easyfood.fragment.HomeFragment.Companion.MEAL_LINK
+import com.example.easyfood.fragment.HomeFragment.Companion.MEAL_NAME
 import com.example.easyfood.fragment.HomeFragment.Companion.MEAL_STR
 import com.example.easyfood.fragment.HomeFragment.Companion.MEAL_THUMB
+import com.example.easyfood.pojo.Meal
+import com.example.easyfood.viewModel.MealViewModel
 
 class MealActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMealBinding
-    private var mealId = ""
-    private var mealStr = ""
-    private var mealThumb = ""
-    private var instruction = ""
-    private var ytUrl = ""
+    private lateinit var mealMvvm : MealViewModel
+
+    private lateinit var mealId : String
+    private lateinit var mealName : String
+    private lateinit var mealThumb : String
+    private lateinit var youtubeLink : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMealBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mealMvvm = ViewModelProvider(this)[MealViewModel::class.java]
+
         getMealInfoFromIntent()
+
         setUpViewWithMealInformation()
+
+        loadingCase()
+        mealMvvm.getMealDetail(mealId)
+        observeMealLiveData()
+
+        onYoutubeImageClick()
+    }
+
+    private fun onYoutubeImageClick() {
+        binding.imgYoutube.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubeLink))
+            startActivity(intent)
+        }
+    }
+
+    private fun observeMealLiveData() {
+        mealMvvm.observeMealDetalisLiveData().observe(this,object :Observer<Meal>{
+            override fun onChanged(value: Meal) {
+                onResponseCase()
+                val meal = value
+
+                binding.tvCategoryInfo.text = "Category : ${meal.strCategory}"
+                binding.tvAreaInfo.text = "Area : ${meal.strArea}"
+                binding.tvContent.text = meal.strInstructions
+
+                youtubeLink = meal.strYoutube
+            }
+        })
     }
 
     private fun setUpViewWithMealInformation() {
@@ -33,30 +78,35 @@ class MealActivity : AppCompatActivity() {
             .load(mealThumb)
             .into(binding.imgMealDetail)
 
-        binding.collapsingToolbar.title = mealStr
+        binding.collapsingToolbar.title = mealName
         binding.collapsingToolbar.setCollapsedTitleTextColor(resources.getColor(R.color.white))
-        binding.tvContent.text = instruction
+        binding.collapsingToolbar.setExpandedTitleColor(resources.getColor(R.color.white))
     }
 
     private fun getMealInfoFromIntent() {
         val tempIntent = intent
 
         mealId = tempIntent.getStringExtra(MEAL_ID) ?: "Null"
-        mealStr = tempIntent.getStringExtra(MEAL_STR) ?: "Null"
-        mealThumb = tempIntent.getStringExtra(MEAL_THUMB) ?: "Null"
-        instruction = demoInstruction
-
+        mealName = tempIntent.getStringExtra(MEAL_NAME).toString()
+        mealThumb = tempIntent.getStringExtra(MEAL_THUMB).toString()
     }
 
-    private var demoInstruction = "Read the recipe thoroughly and ensure each step is clear and makes sense to you.\n" +
-            "\n" +
-            "Purchase your ingredients. It’s a good idea to buy several types of oats for a discussion during or after the presentation. I recommend getting at least three types of oats -- instant, rolled, and steel-cut.\n" +
-            "\n" +
-            "Review my warning about cooking with milk. You’ll find it in Judy’s Tips section below.\n" +
-            "\n" +
-            "Explore the health benefits of oats at the Whole Grain Council's website.\n" +
-            "\n" +
-            "Print any handouts or recipes that you want to distribute to the group.\n" +
-            "\n" +
-            "Practice your demonstration a few times. Try to get family or friends to watch you. Also, we are always just an email or phone call away if you need help. Contact us anytime!"
+    private fun loadingCase(){
+        binding.progressBar.visibility = View.VISIBLE
+        binding.btnSave.visibility = View.INVISIBLE
+        binding.tvInstructions.visibility = View.INVISIBLE
+        binding.tvCategoryInfo.visibility = View.INVISIBLE
+        binding.tvAreaInfo.visibility = View.INVISIBLE
+        binding.imgYoutube.visibility = View.INVISIBLE
+    }
+
+    private fun onResponseCase(){
+        binding.progressBar.visibility = View.INVISIBLE
+        binding.btnSave.visibility = View.VISIBLE
+        binding.tvInstructions.visibility = View.VISIBLE
+        binding.tvCategoryInfo.visibility = View.VISIBLE
+        binding.tvAreaInfo.visibility = View.VISIBLE
+        binding.imgYoutube.visibility = View.VISIBLE
+    }
+
 }
--- app/src/main/java/com/example/easyfood/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/easyfood/fragment/HomeFragment.kt
@@ -2,11 +2,11 @@ package com.example.easyfood.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.easyfood.activities.MealActivity
@@ -28,8 +28,8 @@ class HomeFragment : Fragment() {
         const val CATEGORY_NAME="com.example.easyfood.fragment.categoryName"
         const val MEAL_STR="com.example.easyfood.fragment.strMeal"
         const val MEAL_AREA="com.example.easyfood.fragment.strArea"
-
-
+        const val MEAL_INS="com.example.easyfood.fragment.instruction"
+        const val MEAL_LINK="com.example.easyfood.fragment.youtube"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,6 +61,10 @@ class HomeFragment : Fragment() {
             intent.putExtra(MEAL_ID,randomMeal.idMeal)
             intent.putExtra(MEAL_NAME,randomMeal.strMeal)
             intent.putExtra(MEAL_THUMB,randomMeal.strMealThumb)
+            intent.putExtra(MEAL_AREA,randomMeal.strArea)
+            intent.putExtra(CATEGORY_NAME,randomMeal.strCategory)
+            intent.putExtra(MEAL_INS,randomMeal.strInstructions)
+            intent.putExtra(MEAL_LINK,randomMeal.strYoutube)
             startActivity(intent)
         }
     }
